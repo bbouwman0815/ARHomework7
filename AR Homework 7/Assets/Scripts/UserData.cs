b7c039fd5c8b5fc740b7185891706cea33bb35e8
briairie <birie1@my.westga.edu>
2022-02-20 16:13:07 -0500

    Added form and user data

---
+++ AR Homework 7/Assets/Scripts/UserData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="App Data / User Data")]
+public class UserData : ScriptableObject
+{
+    public string userName;
+    public int age;
+}
